5fb341c6bc55c7ecbd0f6605a2fea139bfddecb1
Muhammad-Faizan3 <fb240951@gmail.com>
2026-02-13 17:56:14 +0500

    first commit

---
+++ Class 38/app.js
@@ -0,0 +1,26 @@
+// Rest operator
+
+// // without Rest Operator
+
+// function sum (a,b,c,...numbers ) {
+//     let alpha = a + b + c;
+//     let sumNumber = numbers.reduce((accumulated +current) => {
+
+//     }) 
+// }
+// console.log(sum(1,2,3));
+
+// with rest operator
+
+// function sum (a,b, ...numbers) {
+//     console.log((a,numbers)); // will recevice array    
+// }
+// sum(1,2,3,4,5);
+
+// let arr = [1,2,3,4,5];
+// let sumNumber = arr.reduce((a,b,c) => {
+//     return a + b + c
+
+// });
+// console.log(sumNumber);
+
