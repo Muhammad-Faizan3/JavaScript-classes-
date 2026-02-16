f804b58e112a538a57b65b3092c2142eb96c45dd
Muhammad-Faizan3 <fb240951@gmail.com>
2026-02-16 16:40:11 +0500

    First Commit

---
+++ Class 39/app.js
@@ -0,0 +1,29 @@
+// let numbers = new Set([1,2,3]);
+
+// console.log(numbers);
+
+// var s = new Set();
+
+// s.add(10);
+// s.add(20);
+// s.add(10); // duplicate
+// s.delete(20)
+// s.clear()
+// console.log(s);
+
+// Loop Through Set 
+
+// let s = new Set([1,2,3]);
+
+// for(let value of s) {
+//     console.log(value);
+    
+// }
+
+// Convert Set to Array (very Important)
+
+// let arr = [1,2,2,3,4];
+
+// let uniqueArr = [...new Set(arr)];
+
+// console.log(uniqueArr);
