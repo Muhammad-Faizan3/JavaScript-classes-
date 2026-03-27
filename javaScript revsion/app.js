46ff51e5918c8d1d59269206a8e2c0aa5d97ba0d
Muhammad-Faizan3 <fb240951@gmail.com>
2026-03-27 17:20:47 +0500

    Revision

--- javaScript revsion/app.js
+++ javaScript revsion/app.js
@@ -1,1 +1,165 @@
-console.log()
+// let a = 12;
+// let b = 5;
+// console.log(a+ ++a - b--);
+
+
+// let firstName = 'Muhammad ';
+// let lastName = 'Faizan';
+// let fullName = firstName + lastName;
+// console.log(fullName);
+
+// console.log(2+2+ '22');
+
+
+
+
+
+// console.log(username)
+
+// temporal dead zone == > TDZ
+let username = "hasancoder66"
+
+
+
+// let userName = "hasanCoder66"
+
+// const USERNAME = "HASANCODER66"
+
+
+
+
+// variable declare
+
+// var mobile;
+
+
+// variable declare and initialize 
+// var mobile = "Samsung"
+// mobile = "Iphone"
+
+
+// initailize kia hota hai ?
+
+// undefined  ===> system generate
+
+// var username;
+
+// console.log(username)
+
+// if(username){
+//     console.log("if true hogiaa ")
+// }else {
+//     console.log("else block chlaa ==>")
+// }
+
+
+// var jo hai redeclare hojata hai + reassign bi hojata hai 
+
+// var declaration;
+
+
+
+// let jo hai redeclare nhi hota. reassign hojata hai.
+// let declaration1 ;
+// declaration1 = "declaration2"
+
+
+
+// const 1 hi dafa declare or initialize hota hai second time koi value accept nhi kerta. 
+
+// const declaration2 = "declaration2"
+// declaration2 = "declaration2"
+
+
+
+
+
+// let a = 12;
+// let b = 5
+
+// console.log(a + ++a - b--)
+
+
+
+// pre increment  
+// post increment   
+
+
+
+
+let firstName = "Muhammad"
+let lastName = "Hasan"
+
+// "", '', ``
+
+// let fullName = firstName + "" + lastName // one
+// let fullName = firstName.concat(" " + lastName) // two
+// let fullName = `${firstName} ${lastName}` // three
+
+// console.log(fullName)
+
+
+// let result = 2 + 2 + 1334578
+// console.log(result.slice(0 , 2))
+
+
+
+
+
+// conditional statement
+
+
+// job requirements ==>   age => 18 , location == karachi , exp == 1, lang == js , qualification == 'matric'
+
+
+// let age = 18;
+// let city = "Karachi"
+// let exp = 1;
+// let qualification = "matric"
+// let lang = "JavaScript"
+
+
+// if(age >= 18){
+// if(city == "Karachi"){
+//   if(exp == 1){
+//       if(lang == "JavaScript"){
+//            if(qualification == "matric"){
+// message(`You Are Elegible for this job!
+//     Congratulations!
+//     `)
+// }else {
+//     message("beta board ke paper ki tayyari kero.")
+// }
+// }else {
+//     message("js seekhloo.")
+// }
+
+// }else {
+//     message("Experience Kerlo life mai.")
+// }
+// }else {
+//     message("Karachi Ajaou")
+// }
+// }else{
+//     message("barry hoker aou")
+// }
+
+
+
+// function message (m){
+//     console.log(m)
+// }
+
+
+
+
+let arr = [12, false, "string", 45, true, {name : "hasan"}]
+
+// arr[5].name = "shakoor"
+
+
+
+
+
+console.log(arr.slice(0,2))
+// console.log(arr)
