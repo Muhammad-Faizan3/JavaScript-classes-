ca9ed2e3ee87a8a9b82d3f34774f33385da06627
Muhammad-Faizan3 <fb240951@gmail.com>
2026-04-28 11:57:11 +0500

    class 02

---
+++ React js/class 02/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Home from './Components/Home';
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <Home/>
+    <div>
+      <h2>The World</h2>
+      <p>Paragraph</p>
+      <img src="" alt="" />
+    </div>
+    </>
+  )
+}
+
+export default App;
---
+++ React js/class 02/src/Components/Home.jsx
@@ -0,0 +1,11 @@
+function Home({name,para}) {
+    return(
+        <>
+      <div className="card">
+      <h2>{name}</h2>
+      <p>{skill}</p>
+    </div>
+        </>
+    )
+} 
+export default Home;
